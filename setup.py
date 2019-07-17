99c3759c0527b3dcbed65edd3c54b9b7fe62f56f
Nitin rai <mneonizer@gmail.com>
2019-07-17 19:58:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'imscreen',         # How you named your package folder (MyLib)
   packages = ['imscreen'],   # Chose the same as "name"
-  version = '0.2.1',      # Start with a small number and increase it with every change you make
+  version = '0.2.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This Library is used to Capture your \"Computer\'s Screen / Webcam\" with Ultra High FPS.',   # Give a short description about your library
   author = 'Nitin Rai',                   # Type in your name
