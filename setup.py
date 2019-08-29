a94b20ad21b0297aa7814e841e5b9a1f1e822acc
Nitin Rai <mneonizer@gmail.com>
2019-08-29 00:51:57 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Nitin Rai',                   # Type in your name
   author_email = 'mneonizer@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/imneonizer/imscreen',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/imneonizer/imscreen/archive/v0.3.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/imneonizer/imscreen/releases/tag/v0.3.0',    # I explain this later on
   keywords = ['Screen Shot', 'Screen Capture', 'Webcam Reading'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'mss',
